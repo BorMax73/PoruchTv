85f96ac5de76842c62e1d99ff5e94942ab8b418f
maxbo <maxbo@BORMAX>
2022-04-05 23:07:55 +0300

    Добавьте файлы проекта.

---
+++ ChatSample/Hubs/ChatHub.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace ChatSample.Hubs
+{
+    public class ChatHub : Hub
+    {
+        internal string ChannelName;
+
+        //public ChatHub(string channelName)
+        //{
+        //    _channelName = channelName;
+        //}
+       
+        public async Task Enter(string groupname, string username)
+        {
+            this.ChannelName = groupname;
+            if (String.IsNullOrEmpty(username))
+            {
+                await Clients.Caller.SendAsync("Notify", "Для входа в чат введите логин");
+            }
+            else
+            {
+                await Groups.AddToGroupAsync(Context.ConnectionId, groupname);
+                await Clients.Group(ChannelName).SendAsync("Notify", $"{username} вошел в чат");
+            }
+        }
+        //public async Task AddToGroup(string groupName)
+        //{
+        //    await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+        //    await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has joined the group {groupName}.");
+        //}
+
+        //public async Task RemoveFromGroup(string groupName)
+        //{
+        //    await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+
+        //    await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has left the group {groupName}.");
+        //}
+        public async Task Play(string command, double seek)
+        {
+            // Call the broadcastMessage method to update clients.
+            await Clients.Group(ChannelName).SendAsync("Play", command, seek);
+        }
+
+        public async Task Pause(string command, double seek)
+        {
+            await Clients.Group(ChannelName).SendAsync("Pause", command, seek);
+        }
+        public async Task SetTime(double time)
+        {
+            await Clients.All.SendAsync("SetTime", time);
+        }
+    }
+}
---
+++ ChatSample/Startup.cs
@@ -0,0 +1,57 @@
+﻿using ChatSample.Hubs;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ChatSample
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        { 
+            services.AddSignalR(options =>
+            {
+                options.EnableDetailedErrors = true;
+            });
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseFileServer();
+
+            app.UseRouting();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapHub<ChatHub>("/chat");
+            //});
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+                endpoints.MapHub<ChatHub>("/chat");
+            });
+        }
+    }
+}
